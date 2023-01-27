0bfb26884eb17e3009fcc214f2c7da168428aea4
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-01-27 15:57:54 +0500

    made Card component as <div> wraper alternative

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,14 +1,15 @@
 import "./ExpenseDate.css";
+import Card from "./Card";
 const ExpenseDate = ({ date }) => {
   const day = date.toLocaleString("en-US", { day: "2-digit" });
   const month = date.toLocaleString("en-US", { month: "long" });
   const year = date.getFullYear();
   return (
-    <div className="expense-date">
+    <Card className="expense-date">
       <div>{day}</div>
       <div>{month}</div>
       <div>{year}</div> 
-    </div>
+    </Card>
   );
 };
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,15 @@
-import { Fragment } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 const ExpenseItem = ({ date, title, amount }) => {
   return (
-    <Fragment>
-      <div className="expense-item">
+      <Card className="expense-item">
         <ExpenseDate date={date} />
         <div className="expense-item__description">
           <h2 className="expense-item h2">{title}</h2>
           <div className="expense-item__price">${amount}</div>
         </div>
-      </div>
-    </Fragment>
+      </Card>
   );
 };
 
