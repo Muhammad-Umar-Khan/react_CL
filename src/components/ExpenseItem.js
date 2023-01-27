d7536d9a72e11db6e3168ec8dfb2c81f7bfef8cf
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-01-27 17:19:26 +0500

    updating states

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,21 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
+
 const ExpenseItem = ({ date, title, amount }) => {
+  function clickHandler() {
+    title = "New title!";
+    console.log(title);
+  }
   return (
-      <Card className="expense-item">
-        <ExpenseDate date={date} />
-        <div className="expense-item__description">
-          <h2 className="expense-item h2">{title}</h2>
-          <div className="expense-item__price">${amount}</div>
-        </div>
-      </Card>
+    <Card className="expense-item">
+      <ExpenseDate date={date} />
+      <div className="expense-item__description">
+        <h2 className="expense-item h2">{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
   );
 };
 
