60b094bf4a6f039b8aa61bfc04a5f1e9ecaa41a6
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-01-27 14:06:48 +0500

    reused components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,18 @@
-const ExpenseItem = () => <h1>This is our first custome made component</h1>;
+import { Fragment } from "react";
+import "./ExpenseItem.css";
+const ExpenseItem = ({date, title, amount}) => {
+  return (
+    <Fragment>
+      <div className="expense-item">
+      {/* {expenseDate} will give you error of can't return objects as JSX element  */}
+        <div>{date.toISOString()}</div> 
+        <div className="expense-item__description">
+          <h2 className="expense-item h2">{title}</h2>
+          <div className="expense-item__price">${amount}</div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
 export default ExpenseItem;
